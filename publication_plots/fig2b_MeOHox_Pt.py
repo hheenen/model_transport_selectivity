c50c351b1ed68f1e05e5b2206c0c9ed3c3ba17ee
hheenen <hheenen@googlemail.com>
2024-01-28 22:31:18 +0100

    finished first plot fig2b

--- publication_plots/fig2b_MeOHox_Pt.py
+++ publication_plots/fig2b_MeOHox_Pt.py
@@ -9,7 +9,82 @@ Methanol oxidation
 
 from header import *
 
+
+def load_literature_MeOHox_Pt_data():
+    """
+      helper-function to load MeOHox data
+
+    """
+    dat_n = np.loadtxt("../literature_data/dat_H2CO_Pt_neg.txt")
+    dat_p = np.loadtxt("../literature_data/dat_H2CO_Pt_pos.txt")
+
+    #### H2CO on Pt oxidation
+    selH2COn, selH2COp = dat_n[:,:2], dat_p[:,:2]
+    selH2COn[:,0] /= selH2COn[:,0].min() # relative loading / roughness
+    selH2COp[:,0] /= selH2COp[:,0].min() # relative loading / roughness
+
+    return selH2COn, selH2COp
     
+
+def plot_MeOHox_Pt(filename, dat, ls_args, sketch):
+    """
+      helper-function to plot MeOHox data
+
+    """
+    set_plotting_env(width=3.25,height=3.25/ golden_ratio *1.05,\
+               lrbt=[0.16,0.98,0.185,0.92],fsize=10)
+    
+    xlabel = r'roughness $\rho$'; ylabel = r'selectivity CH$_2$O (%)'
+    
+    ax = plot_xy_ax(dat, ylabel, xlabel, ls_args, \
+        tag="", line=[], lsize=7)
+    
+    ax.set_xlim(0.7749999999999999, 5.7250000000000005) # hardcoded if model is not included
+    #ax.set_ylim(21.13366790022691, 64.57458724284633) # hardcoded -- no model no neg scan
+    
+    ax.annotate('(b)', xy=(0.04, 0.95), xycoords='figure fraction')
+    
+    if sketch:
+        #add_sketch(ax, r'MeOH', r'HCOOH + CO$_2$', r'H$_2$CO', dxy=(-0.3,-0.45))
+        add_sketch(ax, r'MeOH', r'HCOOH+CO$_2$', r'CH$_2$O', dxy=(0.01,0.15))
+
+    writefig(filename ,folder='output', write_pdf=False, write_png=True, write_eps=False) 
+
+
+def make_plots_MeOHox_Pt():
+    """
+      helper-function to make full plots
+
+    """
+
+    # run model for MeOH ox on Pt example
+    out_plot = run_model_for_example({'Fmdh':cdict}, {'Fmdh':sdict})
+    out_plot['Fmdh'][:,0] /= out_plot['Fmdh'][:,0].min() # relative roughness
+    out_plot['Fmdh'][:,1] *= 100.                        # convert percentage
+
+    # load literature data
+    selH2COn, selH2COp = load_literature_MeOHox_Pt_data()
+
+    ls_args = {r'neg scan':dict(ls='--', marker='x', color='gray'), 
+               r'pos scan':dict(ls='--', marker='x', color='k'), 
+               r'Pt-NP':dict(ls='--', marker='x', color='k'), 
+               r'model':dict(ls='-', color='r'),
+               }
+
+    # plot Fig2b
+    filename = "Fig2b_MeOHox_Pt_SelFmdh"
+    dat = {r'Pt-NP': selH2COp, 
+        r'model':out_plot['Fmdh']}
+    plot_MeOHox_Pt(filename, dat, ls_args, sketch=True)
+    
+    # plot FigS3b
+    filename = "FigS3b_MeOHox_Pt_SelFmdh"
+    dat = {r'neg scan': selH2COn,
+        r'pos scan': selH2COp,
+        r'model':out_plot['Fmdh']}
+    plot_MeOHox_Pt(filename, dat, ls_args, sketch=False)
+
+
 #################################
 ### Parameters for simulation ###
 #################################
@@ -31,70 +106,6 @@ cdict = {'cD':[cD_Fmdh], 'cH':[cH_Fmdh], 'dG':[dG_Fmdh[0]], \
 sdict = {"Us":[-1.60], "rghs":np.arange(0.1,0.6,0.05), "mdls":[1]}
 
 
-
 if __name__ == "__main__":
-
-
-#   def make_plot_MeOHox_Pt():
-#   """
-#     helper-function to make full plot
-
-#   """
-#   pass
-
-    
-
-    ########################
-    ### Simulation block ###
-    ########################
-
-    out_plot = run_model_for_example({'Fmdh':cdict}, {'Fmdh':sdict})
-    out_plot['Fmdh'][:,0] /= out_plot['Fmdh'][:,0].min() # relative roughness
-    out_plot['Fmdh'][:,1] *= 100.                        # convert percentage
-
-    ##########################################
-    ### Plot simulation against experiment ###
-    ##########################################
-
-    #### H2CO on Pt oxidation
-    selH2COn, selH2COp = load_H2CO_Pt_data()
-    selH2COn[:,0] /= selH2COn[:,0].min() # relative loading / roughness
-    selH2COp[:,0] /= selH2COp[:,0].min() # relative loading / roughness
-
-    assert False
-    # ( ) sort out central data here!
-    # ( ) make plot function and have central
-
-    ls_args = {r'neg scan':dict(ls='--', marker='x', color='gray'), 
-               r'pos scan':dict(ls='--', marker='x', color='k'), 
-               r'Pt-NP':dict(ls='--', marker='x', color='k'), 
-               r'model':dict(ls='-', color='r'),
-               }
-    dat_all = {r'neg scan':selH2COn,
-           r'pos scan':selH2COp,
-           r'Pt-NP':selH2COp,
-           r'model':out_plot['Fmdh']
-           }
-    dcoll = {'':[r'Pt-NP',r'model'], '_SI':[r'neg scan', r'pos scan', r'model']}
-
-    for tag in ['', '_SI']:
-        dat = {k:dat_all[k] for k in dcoll[tag]}
-
-        xlabel = r'roughness $\rho$'; ylabel = r'selectivity CH$_2$O (%)'
-        
-        set_plotting_env(width=3.25,height=3.25/ golden_ratio *1.05,\
-                   lrbt=[0.16,0.98,0.185,0.92],fsize=10)
-        
-        ax = plot_xy_ax(dat, ylabel, xlabel, ls_args, tag="", line=[], fsize=10, lsize=7)#, title=r'MeOH oxidation on Pt')
-        ax.set_xlim(0.7749999999999999, 5.7250000000000005) # hardcoded if model is not included
-        #ax.set_ylim(21.13366790022691, 64.57458724284633) # hardcoded -- no model no neg scan
-        ax.annotate('(b)', xy=(0.04, 0.95), xycoords='figure fraction')
-        
-        if tag == '':
-            #add_sketch(ax, r'MeOH', r'HCOOH + CO$_2$', r'H$_2$CO', dxy=(-0.3,-0.45))
-            add_sketch(ax, r'MeOH', r'HCOOH+CO$_2$', r'CH$_2$O', dxy=(0.01,0.15))
- 
-        filename = "ref_MeOHox_Pt_SelFmdh"+tag
-        writefig(filename ,folder='output', write_pdf=False, write_png=True, write_eps=True) 
-
+    make_plots_MeOHox_Pt()
 
--- publication_plots/make_all_plots.py
+++ publication_plots/make_all_plots.py
@@ -7,10 +7,12 @@ Individual scripts generate data and prepare plots
 
 """
 
+from fig2b_MeOHox_Pt import make_plots_MeOHox_Pt
 from figS2_model_parameter_estimate import make_plot_model_parameter_selectivity
 
-
 if __name__ == "__main__":
+    # figure 2b
+    make_plots_MeOHox_Pt
     # figure S2
     make_plot_model_parameter_selectivity()
 
